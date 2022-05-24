b25790bf3893f9439c95d8c6e35d28ecafda9910
Mark Creasy <mcreasy50@gmail.com>
2022-05-24 15:59:00 -0400

    html routing

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+// allow access to public folder
+app.use(express.static('public'));
+
+// parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+// app.use(express.json());
+// Setup Routes
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
