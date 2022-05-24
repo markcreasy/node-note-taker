a4b5b511c32cbb590bc9c5da020f2e3cb470f546
Mark Creasy <mcreasy50@gmail.com>
2022-05-24 16:44:18 -0400

    api - post new note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 // const {filterByQuery, findById, createNewAnimal, validateAnimal} = require('../../lib/animals.js');
 const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  console.log(notes);
   res.json(results);
 });
 
@@ -17,17 +18,34 @@ router.get('/notes', (req, res) => {
 //   }
 // });
 //
-// router.post('/notes', (req, res) => {
-//   // set id based on what the next index of the array will be
-//   req.body.id = notes.length.toString();
-//
-//   // if any data in req.body is incorrect, send 400 error back
-//   if (!validateAnimal(req.body)) {
-//     res.status(400).send('The animal is not properly formatted.');
-//   } else {
-//     const animal = createNewAnimal(req.body, notes);
-//     res.json(animal);
-//   }
-// });
+router.post('/notes', (req, res) => {
+
+  // create new note object
+  let newNote =  { "id": notes.length, "title": req.body.title, "text": req.body.text};
+
+  // assign a new id, making sure not to set a conflicting value
+  notes.forEach((note, i) => {
+    if(note.id > newNote.id){
+      newNote.id = note.id++;
+    }
+  });
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (req.body.title == '' || req.body.text == '') {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    // add new note to the notes array
+    notes.push(newNote);
+
+    // write the notes array to the json file
+    fs.writeFileSync(
+      path.join(__dirname, '../../db/db.json'),
+      JSON.stringify({ notes: notes }, null, 2)
+    );
+
+    // respond with the note array as json
+    res.json(notes);
+  }
+});
 
 module.exports  = router;
